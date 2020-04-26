641b9fd45fbdfc513f2b45cf3a3c628b2015d19d
Sachin Kekarjawalekar <kekarjawalekarsachin1@gmail.com>
2020-04-26 16:28:17 +0530

    Minor documentation additions
    Signed-off-by: Sachin Kekarjawalekar <kekarjawalekarsachin1@gmail.com>

--- main.php
+++ main.php
@@ -2,6 +2,7 @@
 /**
  * This file is executed every time after the required classes are loaded.
  * You can specify constants or global codes!
+ *
  */
 
 namespace ServerlessPHP;
@@ -10,8 +11,7 @@ define("APP_ROOT", __DIR__);
 
 define("APP_TIMEZONE", "Asia/Kolkata");
 
-define("ENVIRONMENT", "Development");
-
+// If SHOW_ERRORS = TRUE the api will show error messages if any in response
 define("SHOW_ERRORS", TRUE);
 
 date_default_timezone_set(APP_TIMEZONE);
